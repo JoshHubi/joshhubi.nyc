07d1e69274c9ff7e2c3934158b1144fbb5d234ba
Josh Hubi <me@joshhubi.com>
2022-04-06 03:59:43 -0400

    Added 404 page.

--- client/src/pages/not-found/index.tsx
+++ client/src/pages/not-found/index.tsx
@@ -6,15 +6,20 @@
  */
 
 import React from 'react'
-// import { ReactTitle } from 'react-meta-tags'
+import { Helmet } from 'react-helmet'
 
 import styles from './style.module.scss'
 
 export default function home () {
 	return (
 		<div className={styles.home}>
-			{/* <ReactTitle title='Page Not Found - Josh Hubi'/> */}
-			{`help computer`}
+			<Helmet>
+                <title>Page not found - Josh Hubi</title>
+            </Helmet>
+			<div className="p-16">
+				<h2 className="text-7xl text-center font-semibold">404</h2>
+				<p className="text-center text-3xl mt-2">The page you're looking for is not here but this cat is <img className="inline-block align-bottom h-10 rounded-full sm:mx-0 sm:shrink-0 hover:scale-125 transition-all" alt="Cat" src="https://media0.giphy.com/media/jpbnoe3UIa8TU8LM13/200w.webp" /></p>
+			</div>
 		</div>
 	)
 }
