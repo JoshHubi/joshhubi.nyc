22cf3c86bb4be99d167079ed7a524dc2dba09524
Josh Hubi <me@joshhubi.com>
2022-04-06 04:00:15 -0400

    Created reactive menu navigation

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -2,30 +2,44 @@
  * @file Header component
  *
  * @author Josh Hubi
- * @since 6/1/2021
+ * @since 4/6/2022
  */
 
 import React from 'react'
+
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+import type { LinkProps } from 'react-router-dom'
+
 import { ReactComponent as Logo } from 'assets/img/logo.svg'
-// import styles from './style.module.scss'
+
+function CustomLink({ children, to, ...props }: LinkProps) {
+	let resolved = useResolvedPath(to)
+	let match = useMatch({ path: resolved.pathname, end: true })
+
+	return (
+		<li>
+			<Link
+			className={match ? "text-purple" : "hover:text-purple"}
+			to={to}
+			{...props}
+		>
+			{children}
+			</Link>
+		</li>
+	)
+}
 
 export default function Header() {
 	return (
 		<div className="w-full sm:px-12 py-7 flex items-center">
 			<div className="flex-1">
-				<Logo />
+				<Link to="/"><Logo /></Link>
 			</div>
 			<div className="justify-end text-lg font-bold pr-2">
 				<ul className="flex space-x-7">
-					<li>
-						<a className="text-purple" href="#">Work</a>
-					</li>
-					<li>
-						<a className="hover:text-purple" href="#">About</a>
-					</li>
-					<li>
-						<a className="hover:text-purple" href="#">Contact</a>
-					</li>
+					<CustomLink to="/">Work</CustomLink>
+					<CustomLink to="/about">About</CustomLink>
+					<CustomLink to="/contact">Contact</CustomLink>
 				</ul>
 			</div>
 		</div>
