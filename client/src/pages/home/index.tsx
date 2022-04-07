95523e719402b7528ed46d9b42c0cfd59baf5f9f
Josh Hubi <me@joshhubi.com>
2022-04-07 03:29:29 -0400

    TABS!

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -19,14 +19,14 @@ export default function home() {
 	return (
 		<div className={styles.home}>
 			<Helmet>
-                <title>I am Josh Hubi</title>
-            </Helmet>
+				<title>I am Josh Hubi</title>
+			</Helmet>
 			<div className="font-crimson py-20 max-w-[59rem] m-auto relative overflow-hidden p-5">
 				<img className={styles.moon} alt="moon" src={moon} />
 				<div className="text-center max-w-[42rem] m-auto text-[2.5rem] leading-[2.8rem]">
 					I am Josh Hubi <Link to="/about">
 						<img className="cursor-pointer inline-block align-bottom h-10 rounded-full sm:mx-0 sm:shrink-0 hover:scale-125 transition-all" alt="Josh Hubi" src={me} />
-						</Link>, a full stack developer and producer crafting <strong><em className="font-crimson italic">digital products</em></strong>
+					</Link>, a full stack developer and producer crafting <strong><em className="font-crimson italic">digital products</em></strong>
 				</div>
 			</div>
 
