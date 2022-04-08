4218bd659af3b48509bca391fd55b3fc2931f2b2
Josh Hubi <me@joshhubi.com>
2022-04-08 06:28:29 -0400

    Added lint config.

---
+++ client/.eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": 12,
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react"
+	],
+	"rules": {
+		'padded-blocks': 0,
+		'spaced-comment': 0, // prevent spaced comment diagrams
+		'indent': [1, 'tab', { 'SwitchCase': 1 }] // set indenation to tabs and enable for switch statements
+	}
+}
